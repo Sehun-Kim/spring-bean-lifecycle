0f06d4772da823a4d6a5317ea456baae18d12a1f
sechun <sechun0215@gmail.com>
2018-12-02 23:58:19 +0900

    scopse modified

--- test/src/main/java/spring/springbeanscope/ScopeOneController.java
+++ test/src/main/java/spring/springbeanscope/ScopeOneController.java
@@ -5,8 +5,8 @@
 //import org.springframework.web.bind.annotation.RestController;
 //
 //@RestController
-//@RequestMapping("/scopes")
-//public class ScopeController {
+//@RequestMapping("/scopesOne")
+//public class ScopeOneController {
 //
 //    @Autowired
 //    SingleBean singleBean;
@@ -18,14 +18,14 @@
 //    public String single() {
 //        System.out.println("single => Singleton : " + singleBean.getName());
 //        System.out.println("single => Prototype : " + protoBean.getName());
-//        return "singleton";
+//        return singleBean.toString();
 //    }
 //
 //    @RequestMapping("/proto")
 //    public String proto() {
 //        System.out.println("proto => Singleton : " + singleBean.getName());
 //        System.out.println("proto => Prototype : " + protoBean.getName());
-//        return "prototype";
+//        return protoBean.toString();
 //    }
 //
 //}
